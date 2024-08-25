ebdeca2484ec0923ac9f3746fc87102f24e2681b
Afkar <mafkartriw@gmail.com>
2024-08-25 20:42:29 +0700

    .

---
+++ config/services.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'slack' => [
+        'notifications' => [
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+        ],
+    ],
+    'google' => [
+        'client_id' => ('446426747430-o4ukvslhkf3g97v1rc9gp5steunqtqvh.apps.googleusercontent.com'),
+        'client_secret' => ('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'),
+        'redirect' => ('https://v3422031.mhs.d3tiuns.com/authorized/google/callback'),
+    ],
+
+];
